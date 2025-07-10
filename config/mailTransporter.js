622dea59f4e9e694cbaf64b14e396fdfcb71d35c
Martina Schaller <martinaschaller12@gmail.com>
2025-07-10 10:29:42 -0300

    logs de user mail

--- config/mailTransporter.js
+++ config/mailTransporter.js
@@ -9,3 +9,8 @@ export const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS,
   },
 });
+
+console.log("Mail transporter configured:", {
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  user: process.env.EMAIL_USER,});
